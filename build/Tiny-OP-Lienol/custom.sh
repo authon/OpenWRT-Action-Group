fdde866a03769a31d2fee7a07994c03dc7c476c6
authon <authon@vip.qq.com>
2021-01-11 00:43:47 +0800

    Update custom.sh

--- build/Tiny-OP-Lienol/custom.sh
+++ build/Tiny-OP-Lienol/custom.sh
@@ -10,6 +10,7 @@ cat feeds.conf.default
 
 # 添加第三方软件包
 git clone https://github.com/authon/authon-openwrt-hub.git -b 19.07 package/authon-openwrt-hub
+svn co https://github.com/openwrt/luci/trunk/openwrt-19.07/collections/luci-ssl-openssl package/diy/luci-ssl-openssl #uhttpd启用https依赖
 
 # 更新并安装源
 ./scripts/feeds clean
@@ -18,7 +19,6 @@ git clone https://github.com/authon/authon-openwrt-hub.git -b 19.07 package/auth
 # 为19.07添加libcap-bin依赖
 rm -rf feeds/packages/libs/libcap
 svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
-svn co https://github.com/openwrt/luci/trunk/openwrt-19.07/collections/luci-ssl-openssl package/diy/luci-ssl-openssl
 
 # 自定义定制选项
 sed -i 's#192.168.1.1#10.10.10.1#g' package/base-files/files/bin/config_generate #定制默认IP
