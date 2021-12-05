1c02a9ff0f3f73c6325f4d7ce2c249bdb1653190
authon <authon@vip.qq.com>
2021-12-05 17:20:33 +0800

    Update custom.sh

--- build/Tiny-OP/custom.sh
+++ build/Tiny-OP/custom.sh
@@ -117,15 +117,15 @@ EOF
 
 
 # dnsmasq启用
-# cat >> .config <<EOF
-# CONFIG_PACKAGE_dnsmasq_full=y
-# CONFIG_PACKAGE_dnsmasq_full_ipset=y
-# CONFIG_PACKAGE_kmod-ipt-ipset=y
-# # CONFIG_PACKAGE_kmod-sched-ipset is not set
-# CONFIG_PACKAGE_ipset=y
-# # CONFIG_PACKAGE_ipset-dns is not set
-# CONFIG_PACKAGE_libipset=y
-# EOF
+cat >> .config <<EOF
+CONFIG_PACKAGE_dnsmasq_full=y
+CONFIG_PACKAGE_dnsmasq_full_ipset=y
+CONFIG_PACKAGE_kmod-ipt-ipset=y
+# CONFIG_PACKAGE_kmod-sched-ipset is not set
+CONFIG_PACKAGE_ipset=y
+# CONFIG_PACKAGE_ipset-dns is not set
+CONFIG_PACKAGE_libipset=y
+EOF
 
 # IPv6支持:
 # cat >> .config <<EOF
