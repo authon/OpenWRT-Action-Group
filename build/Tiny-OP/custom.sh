37e5e8b0cf13fb3faa3d703ded35a327e8f01c14
authon <authon@vip.qq.com>
2023-10-03 20:49:19 +0800

    Update custom.sh

--- build/Tiny-OP/custom.sh
+++ build/Tiny-OP/custom.sh
@@ -5,7 +5,7 @@
 
 # 更新feeds文件
 sed -i 's@#src-git helloworld@src-git helloworld@g' feeds.conf.default #启用helloworld
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 cat feeds.conf.default
 
