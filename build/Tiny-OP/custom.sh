10e099d895f466da0a659b181369e722692791d6
authon <authon@vip.qq.com>
2023-10-08 11:24:29 -0500

    Update custom.sh

--- build/Tiny-OP/custom.sh
+++ build/Tiny-OP/custom.sh
@@ -28,9 +28,9 @@ rm -rf feeds/luci/themes/luci-theme-argon
 pushd feeds/packages/lang
 rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-23.05/lang/golang
 popd 
-pushd feeds/packages/net
-rm -rf haproxy && svn co https://github.com/kenzok8/small-package/trunk/haproxy
-popd
+# pushd feeds/packages/net
+# rm -rf haproxy && svn co https://github.com/kenzok8/small-package/trunk/haproxy
+# popd
 # rm -rf package/lean/luci-app-sfe
 # rm -rf package/lean/luci-app-flowoffload
 # rm -rf package/lean/luci-app-turboacc
